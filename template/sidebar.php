478c97dd30f6e6496dd4eaacd681df0558afe8a8
MENNNNN21 <lukmannulhakim21lk@gmail.com>
2026-04-14 16:06:25 +0700

    update#3

--- template/sidebar.php
+++ template/sidebar.php
@@ -1,33 +1,49 @@
+<!-- Navbar -->
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <span class="navbar-brand">📚 Toko Buku</span>
+</nav>
+
+<!-- Sidebar -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="#" class="brand-link">
-        <span class="brand-text font-weight-light">Toko Buku</span>
+    <a href="#" class="brand-link text-center">
+        <span class="brand-text font-weight-light">Admin</span>
     </a>
+
     <div class="sidebar">
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+        <nav>
+            <ul class="nav nav-pills nav-sidebar flex-column">
+
                 <li class="nav-item">
                     <a href="../dashboard.php" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <i class="nav-icon fas fa-home"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
+
                 <li class="nav-item">
                     <a href="../buku/index.php" class="nav-link">
                         <i class="nav-icon fas fa-book"></i>
                         <p>Data Buku</p>
                     </a>
                 </li>
+
+                <li class="nav-item">
+                    <a href="../peminjaman/index.php" class="nav-link">
+                        <i class="nav-icon fas fa-box"></i>
+                        <p>Peminjaman</p>
+                    </a>
+                </li>
+
                 <li class="nav-item">
                     <a href="../logout.php" class="nav-link">
                         <i class="nav-icon fas fa-sign-out-alt"></i>
                         <p>Logout</p>
                     </a>
                 </li>
+
             </ul>
         </nav>
     </div>
 </aside>
-<div class="content-wrapper">
-    <section class="content-header"></section>
-    <section class="content">
-        <div class="container-fluid"></div>
+
+<div class="content-wrapper p-4">
